94ca35ad1e5480516df46e4c76564803c466712d
victorfernandesraton <vfbraton@gmail.com>
2021-03-27 15:05:17 -0300

    implement username functionality

--- src/components/User/User-utils.js
+++ src/components/User/User-utils.js
@@ -1,3 +1,28 @@
 export const parseUsername = (user) => {
-  return "";
+  let first_name = "",
+    last_name = "",
+    username = "";
+
+  if (user?.first_name && typeof user?.first_name == "string") {
+    first_name = user?.first_name;
+  }
+
+  if (user?.last_name && typeof user?.last_name == "string") {
+    if (user?.last_name?.length > 8) {
+      last_name = `${user.last_name[0]}.`;
+    } else {
+      last_name = user.last_name.trim();
+    }
+  }
+
+  if (user?.username && typeof user?.username == "string") {
+    username = user.username;
+    if (first_name || last_name) {
+      return `${first_name} ${last_name} (${username})`.trim();
+    } else {
+      return `${username}`;
+    }
+  } else {
+    return `${first_name} ${last_name}`.trim();
+  }
 };
--- src/components/User/test/User-utils.test.js
+++ src/components/User/test/User-utils.test.js
@@ -5,11 +5,46 @@ describe("User-utils", () => {
     test("should have firstName, lastName and username", () => {
       const user = {
         first_name: "Victor",
-        last_name: "Raton",
+        last_name: "Baiao",
         username: "vraton21",
       };
       const result = parseUsername(user);
-      expect(result).toStrictEqual("Victor Raton (vraton21)");
+      expect(result).toStrictEqual("Victor Baiao (vraton21)");
+    });
+
+    test("should have firstname, lastname, usrname and lstname have more than 8 characters", () => {
+      const user = {
+        first_name: "Victor",
+        last_name: "Fernandes",
+        username: "vraton21",
+      };
+      const result = parseUsername(user);
+      expect(result).toStrictEqual("Victor F. (vraton21)");
+    });
+
+    test("shoud have only username", () => {
+      const user = {
+        username: "jeanJesus21",
+      };
+      const result = parseUsername(user);
+      expect(result).toStrictEqual("jeanJesus21");
+      expect(user).not.toBe("(");
+      expect(user).not.toBe(")");
+    });
+
+    test("should have only lastname", () => {
+      const user = {
+        last_name: "Raton",
+      };
+      const result = parseUsername(user);
+      expect(result).toStrictEqual("Raton");
+    });
+    test("should have only lastname and its have more than 8 characters", () => {
+      const user = {
+        last_name: "Fernandes",
+      };
+      const result = parseUsername(user);
+      expect(result).toStrictEqual("F.");
     });
   });
 });
